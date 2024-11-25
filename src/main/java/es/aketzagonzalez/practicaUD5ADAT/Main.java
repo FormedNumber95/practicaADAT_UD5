3eff2a0cd775e042b1b397db92ab98103ed79083
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-11-25 08:39:05 +0100

    Initial commit

---
+++ src/main/java/es/aketzagonzalez/practicaUD5ADAT/Main.java
@@ -0,0 +1,21 @@
+package es.aketzagonzalez.practicaUD5ADAT;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+        System.out.println("1:\tListado de deportistas participantes:");
+        System.out.println("2:\tModificar medalla deportista:");
+        System.out.println("3:\tAñadir deportista/participación::");
+        System.out.println("4:\tEliminar participación:");
+        System.out.println("0:\tSALIR:");
+        int respuesta=input.nextInt();
+        input.nextLine();
+        switch(respuesta) {
+        
+        }
+	}
+
+}
